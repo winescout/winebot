305f299bf1a174930daefa7b773cbb6b17364407
Matt Clark <winescout@gmail.com>
2008-12-12 08:59:14 -0600

    added datamapper and some models

--- winebot.rb
+++ winebot.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'twitter'
+require 'dm-core'
 
 module Winebot
   def self.last_id
@@ -17,6 +18,7 @@ module Winebot
   end
   
   def self.runner
+    DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/winebot.db")
     Twitter::Search.new.since(self.last_id).to('testtobot').each do |new_request|
       self.send_response(new_request)
       self.set_last_id(new_request["id"])
@@ -40,6 +42,34 @@ module Winebot
     def to_s #this is where the cleverness of the parser is revealed in a brililant wine choice
       "xxxyyyy"
     end
+  end
+
+  class Wine
+    include DataMapper::Resource
+    property :id,         Integer, :serial => true
+    property :text,       String
+    property :url,        String
+    property :created_at, DateTime
+    has n, :suggestions
+    has n, :entrees, :through => :suggestions
+  end
+
+  class Entree
+    include DataMapper::Resource
+    property :id,         Integer, :serial => true
+    property :name,       String
+    property :created_at, DateTime
+    has n, :suggestions
+    has n, :wines, :through => :suggestions
+  end
+
+  class Suggestion
+    include DataMapper::Resource
+    property :id,         Integer, :serial => true
+    property :created_at, DateTime
+    property :is_active,  Boolean
+    belongs_to :entree
+    belongs_to :wine
   end
 end
 
